deb9d82a7dcd4eea07fbb37bafbe9ab0861a8393
ashwathymanimala <mv.ashwathy@gmail.com>
2022-09-27 14:21:17 +0530

    calculator

--- ecomm/urls.py
+++ ecomm/urls.py
@@ -18,5 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ecomm/',include("home.urls"))
+    path('ecomm/',include("home.urls")),
+    path('calculator/',include("calculator.urls")),
 ]
